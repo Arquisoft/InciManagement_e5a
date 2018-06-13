8032dd71a75b1839dcdf31d21dd00c55b26a073f
guillediaz11 <guilledp11@gmail.com>
2018-06-13 20:12:46 +0200

    empezando dashboard

--- src/main/java/asw/dbManagement/model/IncidenceStatus.java
+++
@@ -1,6 +0,0 @@
-package asw.dbManagement.model;
-
-public enum IncidenceStatus {
-
-	OPENED, IN_PROCESS, CLOSED, CANCELLED
-}
--- src/main/java/asw/incidenceController/CreateIncidence.java
+++
@@ -1,8 +0,0 @@
-package asw.incidenceController;
-
-import asw.dbManagement.model.Incidence;
-
-public interface CreateIncidence {
-
-	Incidence createIncidence(String user, String pass, String name, String description);
-}
